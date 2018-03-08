457f0c77aa4b29a440d5cc8075428d185054afce
Jeff Handley <jeff.handley@concur.com>
2018-03-07 16:39:27 -0800

    fix: remove logging

--- setColor.js
+++ setColor.js
@@ -1,9 +1,6 @@
 const Luxafor = require('node-luxafor');
 const light = new Luxafor();
-const fs = require('fs');
 
 module.exports = function setColor(red, green, blue) {
-    fs.writeFileSync('/Users/i848715/code/node-luxafor-cli/setColor.log', JSON.stringify({red, green, blue}));
-
     light.color(red, green, blue).exec();
 }
